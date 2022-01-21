2497890abdd78d084e6111689ae9cba87d4b80c7
Oliver <oliverthorpe509@hotmail.co.uk>
2022-01-21 17:19:51 +0000

    Initial commit for about me page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { SkillCardComponent } from './components/shared/skill-card/skill-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    SkillCardComponent
   ],
   imports: [
     BrowserModule
