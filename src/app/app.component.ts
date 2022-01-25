32535cadbc54c165268cd069c71036ac67e9048d
Oliver Thorpe <41513368+OJT509@users.noreply.github.com>
2022-01-25 21:10:26 +0000

    Added help prompt for snap scroll (#4)
    Help dialog implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { HelpPromptComponent } from './components/shared/help-prompt/help-prompt.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Oliver Thorpe';
+
+  helpMessage: string = 
+  `This website uses smooth scrolling, which is disabled by default in some browsers. 
+  To experience these smooth transitions, please enable them in your browser. 
+  Please note that this will not impact your overall use of the website
+  if you opt to not enable this feature.`;
+
+  constructor(public dialog: MatDialog) { }
+
+  helpDialog(): void {
+    this.dialog.open(HelpPromptComponent, {
+      height: '350px',
+      width: '600px',
+      panelClass: 'frosted-dialog',
+      data: this.helpMessage
+    });
+  }
 }
